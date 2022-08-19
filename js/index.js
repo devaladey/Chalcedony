3acfd79e0d6b49f8e2df34ce71e9bc28376333f9
unknown <oluwatobioyedokun7@gmail.com>
2022-08-19 18:40:05 +0100

    first commit

--- js/index.js
+++ js/index.js
@@ -3,5 +3,22 @@ const toggleMenu = document.querySelector('.navbar-menu');
 
 toggleBtn.addEventListener('click', function() {
     toggleMenu.classList.toggle("active");
+    toggleBtn.classList.toggle('active');
 });
+
+const faqTopDiv = document.querySelectorAll('.faq-col-inner');
+
+faqTopDiv.forEach(el=> {
+    el.addEventListener('click', function(e) {
+        if(e.target.classList.contains('faq-btn')) {
+            if(this.nextElementSibling.style.display === "none") {
+                this.nextElementSibling.style.display = "block";
+                e.target.innerHTML = "-";
+            } else {
+                this.nextElementSibling.style.display = "none";
+                e.target.innerHTML = "+";
+            }
+        }
+    });
+})
 
